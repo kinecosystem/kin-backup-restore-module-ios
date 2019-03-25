dfe98325a1b11cc1372edeae3eaffcf77eed21cf
Corey Werner <cnotethegr8@gmail.com>
2019-03-25 10:00:18 +0200

    organized files

---
+++ KinBackupRestoreModule/KinBackupRestoreModule/Views/ExplanationTemplate/ExplanationTemplateView.swift
@@ -0,0 +1,9 @@
+//
+//  ExplanationTemplateView.swift
+//  KinBackupRestoreModule
+//
+//  Created by Corey Werner on 25/03/2019.
+//  Copyright © 2019 Kin Foundation. All rights reserved.
+//
+
+import Foundation
